ea5fdcbd7599daf2dcf4e45c740d47216e3b38ba
janvipatil585-collab <janvipatil585@gmail.com>
2025-12-14 11:10:38 +0530

    code in python

---
+++ #Traffic lights code.py
@@ -0,0 +1,12 @@
+#Traffic lights code
+
+light= input('light :')
+if(light == 'red' ):
+    print('stop')
+elif(light == 'yellow'):
+    print('look')
+elif(light =='green'):
+    print('go')
+else:
+    print('light is broken')
+    
